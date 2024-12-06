a53fd531d09147cd42258492ef8d4f32cf2198e3
Paul Skamrada <pl.skamrada@gmail.com>
2024-12-06 16:22:13 +0100

    Solve part 2

--- day_06/solution.go
+++ day_06/solution.go
@@ -14,30 +14,33 @@ type day struct{}
 
 func (d *day) SolvePart1(content string) (fmt.Stringer, error) {
 	x, y, grid := parse(content)
-	direction := 0
+	_, visited := solve(x, y, 0, grid)
 
-	positions := []int{linear(x, y, len(grid))}
-	for {
-		newX, newY := move(x, y, direction)
-		if !inbounds(newX, newY, len(grid)) {
-			break
-		}
-
-		if grid[newY][newX] {
-			direction = (direction + 1) % 4
-			continue
-		}
-		x, y = newX, newY
-		positions = append(positions, linear(x, y, len(grid)))
-	}
-
-	count := len(removeDuplicates(positions))
-
+	count := len(visited)
 	return solution.New(count), nil
 }
 
 func (d *day) SolvePart2(content string) (fmt.Stringer, error) {
-	return nil, fmt.Errorf("Not yet implemented")
+	x, y, grid := parse(content)
+	_, visited := solve(x, y, 0, grid)
+
+	count := 0
+	for position := range visited {
+		if position.x == x && position.y == y {
+			continue
+		}
+
+		grid[position.y][position.x] = true
+
+		loop, _ := solve(x, y, 0, grid)
+		if loop {
+			count += 1
+		}
+
+		grid[position.y][position.x] = false
+	}
+
+	return solution.New(count), nil
 }
 
 func parse(content string) (x int, y int, grid [][]bool) {
@@ -61,29 +64,12 @@ func parse(content string) (x int, y int, grid [][]bool) {
 	return x, y, grid
 }
 
-func linear(x int, y int, size int) int {
-	return y*size + x
-}
-
 func inbounds(x int, y int, size int) bool {
 	inbound := func(coordinate int) bool {
 		return coordinate >= 0 && coordinate < size
 	}
 
 	return inbound(x) && inbound(y)
-}
-
-func removeDuplicates(positions []int) []int {
-	values := make(map[int]bool)
-	list := make([]int, 0)
-	for _, value := range positions {
-		if _, exists := values[value]; !exists {
-			values[value] = true
-			list = append(list, value)
-		}
-	}
-
-	return list
 }
 
 func move(x int, y int, direction int) (newX int, newY int) {
@@ -98,5 +84,67 @@ func move(x int, y int, direction int) (newX int, newY int) {
 		return x - 1, y
 	default:
 		return x, y
+	}
+}
+
+func solve(x int, y int, direction int, grid [][]bool) (loop bool, positions map[position]bool) {
+	traversed := make(map[traversal]bool)
+	traversed[traverse(x, y, direction)] = true
+	for {
+		newX, newY := move(x, y, direction)
+		if !inbounds(newX, newY, len(grid)) {
+			break
+		}
+
+		if _, exists := traversed[traverse(newX, newY, direction)]; exists {
+			return true, nil
+		}
+
+		if grid[newY][newX] {
+			direction = (direction + 1) % 4
+			continue
+		}
+
+		x, y = newX, newY
+
+		traversed[traverse(x, y, direction)] = true
+	}
+
+	return false, simpleMap(traversed)
+}
+
+func simpleMap(visited map[traversal]bool) map[position]bool {
+	out := make(map[position]bool, len(visited))
+	for traversal := range visited {
+		out[traversal.pos] = true
+	}
+
+	return out
+}
+
+type position struct {
+	x int
+	y int
+}
+
+func at(x int, y int) position {
+	return position{
+		x: x,
+		y: y,
+	}
+}
+
+type traversal struct {
+	pos position
+	dir int
+}
+
+func traverse(x int, y int, dir int) traversal {
+	return traversal{
+		pos: position{
+			x: x,
+			y: y,
+		},
+		dir: dir,
 	}
 }
